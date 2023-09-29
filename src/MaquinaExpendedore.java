59e75cae21148c9125fc9594b3c31b1562ddd849
Fabio-Romero <145377565+Fabio-Romero@users.noreply.github.com>
2023-09-28 22:26:10 -0500

    cambios en el metodo pedir golosinas     cambio en las condiciones

--- src/MaquinaExpendedore.java
+++ src/MaquinaExpendedore.java
@@ -25,32 +25,37 @@ public class MaquinaExpendedore {
             { 5, 5, 5, 5 }
     };
 
-    public MaquinaExpendedore() {
-        apagada = false;
-    }
 
-    public void pedirGolosina() {
+    public boolean pedirGolosina() {
         String pos;
         System.out.println("Introduce la posición de la golosina que quieres (fila, columna): ");
         pos = sc.nextLine();
+        int fila, columna;
         try {
+
             if (pos.length() == 2) {
-                int fila = Integer.parseInt(pos.substring(0, 1));
-                int columna = Integer.parseInt(pos.substring(1));
-                System.out.println("Introduce el dinero: ");
-                double dinero = sc.nextDouble();
+                fila = Integer.parseInt(pos.substring(0, 1));
+                columna = Integer.parseInt(pos.substring(1));
+            }else{
+                System.out.println("Posición incorrecta");
+                return false;
+            }
+
+            System.out.println("Introduce el dinero: ");
+            double dinero = sc.nextDouble();
                 if (cantidad[fila][columna] > 0 && dinero >= precio[fila][columna]) {
-                    double cambio = dinero > precio[fila][columna] ? (int) (dinero - precio[fila][columna]) : 0;
+                    double cambio =  (int) (dinero - precio[fila][columna]);
                     System.out.println("Aquí tiene su golosina: " + nombresGolosinas[fila][columna]);
+                    cantidad[fila][columna]--;
                     System.out.println("Precio: $" + precio[fila][columna] + " Cambio: $" + cambio);
+                    return true;
                 } else {
                     System.out.println("No hay golosinas o no hay dinero suficiente");
+                    return false;
                 }
-            } else {
-                System.out.println("Posición incorrecta");
-            }
         } catch (Exception e) {
             System.out.println("posición incorrecta");
+            return false;
         }
 
     }
